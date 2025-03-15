b85aacfa820f57f5e55d0d0c7429ea7ef923d421
Chakhrisss1102 <saumax1102@gmail.com>
2025-03-15 20:59:22 +0700

    Study07-09

--- study06.js
+++ study06.js
@@ -3,10 +3,13 @@
 //------------------------------
 // Assignment Operators = += -= *= /= %=
 let a = 10
-let b = b + a // หรือ  b+=a
-a = a * 100 + b - 3 // หรือ a *= 100 + b - 3
+let b = 20 // หรือ b+=a
+a = a *100 + b - 3 // หรือ a *= 100 + b - 3
 
-// Nullish Coalescing Operator __ ?? __ 
+
+
+
+// Nullish Coalescing Operator _ ?? _ 
 // กรณีที่หน้า ?? เป็น null จะได้ค่าหลังเครื่องหมาย ??
 // กรณีที่หน้า ?? เป็นใช่ null จะได้ค่านั้นๆ เลย
 let aa = null
@@ -14,39 +17,27 @@ let bb = 30
 console.log(aa ?? 'Hello')
 console.log(bb ?? 555)
 
-
-// Ternary Conditional Operator __?__:__
-//เงื่อนไข ? ทำ/ได้ เมื่อจริง : ทำ/ได้ เมื่อไม่จริง
-console.log(100 < 500 ? 'Hello' : 'Bye')
+// Ternary Conditional Operator _?_:__
+// เงื่อนไข ? ทำ/ได้ เมื่อจริง : ทำ/ได้ เมื่อเท็จ
+console.log(100 < 500 ? 'Hello...' : 'Bye...')
 let cc = 'Money'
 let dd = 'Monkey'
-console.log(cc == dd ? 'Woow....' : 'Woo.......')
+console.log(cc == dd ? 'WOW...' : 'Woo...')
 
-
-
-// Spread Operator __...__ จะใช้กับ object และ array
-let ee = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-let ff = [111, 222]
-console.log('------------------')
+// Spread Operator _..._ จะใช้กับ object และ array
+let ee = [10,20,30,40,50,60,70,80,90,100]
+let ff = [111,222]
+console.log('-----------------------------')
 console.log(ee)
 console.log(ff)
-let gg = [...ee, ...ff]
+let gg = [...ee,...ff]
 console.log(gg)
-console.log('------------------')
-let hh = {data1: 'Hello', data2: 'Bye'}
-let ii = {data3: 'Hi', data4: 'Bye'}
+let hh = [555,999,...ee,888,...ff]
 console.log(hh)
-console.log(ii)
-let jj = {...hh, ...ii}
-console.log(jj)
-let hh =[555, 999, ...ee, ...ff]
-console.log(hh)
-console.log('------------------')
+console.log('-----------------------------')
 
-let ii = {data1: 'Hello', data2: 'Bye'}
-let jj = {data3: 'Money', ...ii}
+let ii = {data1:'Hi...',data2:'Bye...'}
+let jj = {data3:'Money',...ii}
 console.log(jj)
-let kk = {data4: 'Money', ...ii}
+let kk = {data3:'Money',ii}
 console.log(kk)
-
-
